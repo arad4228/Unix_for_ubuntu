e51bdd0c3ee5e5951ec6c42b0f857d54b219f1a6
arad4228 <arad4228@gmail.com>
2021-11-28 11:09:45 +0900

    13주차 실습 코드

---
+++ 13-x/11-2.c
@@ -0,0 +1,20 @@
+#include <netdb.h>
+#include <stdio.h>
+
+int main(void) 
+{
+    	struct servent *port;
+    	int n;
+
+    	setservent(0);
+
+    	for (n = 0; n < 10; n++) 
+	{
+        	port = getservent();
+        	printf("Name=%s, Port=%d\n", port->s_name, port->s_port);
+    	}
+
+    	endservent();
+
+    	return 0;
+}
