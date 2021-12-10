3e842a9b2636138830f0510d0c42e58aa506b3e3
arad4228 <arad4228@gmail.com>
2021-12-10 10:02:11 +0900

    프로젝트 중간업로드

---
+++ project2/client.c
@@ -0,0 +1,125 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SOCKET_NAME   "TodolistProject"
+
+int main(void) 
+{
+    	int sd, len;
+	// 반복문을 끝내기 위한 장치이며 사용자의 입력을 받아줄 변수
+    	char command;
+	struct sockaddr_un ser;
+	
+	// 소켓을 생성한다.
+    	if ((sd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) 
+	{
+		// 만약 오류가 발생하면 에러를 출력하고 종료
+        	perror("socket");
+        	exit(1);
+    	}	
+	
+	// 메모리를 초기화 하고 설정값들을 복사한다.
+    	memset((char *)&ser, '\0', sizeof(ser));
+    	ser.sun_family = AF_UNIX;
+	// 소켓 주소 구조체에 값 저장
+    	strcpy(ser.sun_path, SOCKET_NAME);
+    	len = sizeof(ser.sun_family) + strlen(ser.sun_path);
+
+	// 서버와 연결을 요청한다.
+    	if (connect(sd, (struct sockaddr *)&ser, len) < 0) 
+	{
+		// 서버와 접속이 실패했다면 오류를 출력후 종료
+        	perror("connection error");
+        	exit(1);
+    	}
+	printf("서버와 연결중 ....\n");
+	while(1)
+	{
+		printf("원하는 메뉴를 고르세요.\n");
+		printf("1. 할일 저장하기\n");
+		printf("2. 할일 지우기\n");
+		printf("3. 할일 출력하기(모두)\n");
+		printf("4. 할일 출력하기(오늘)\n");
+		printf("5. 종료(Q,q)\n"); 
+		scanf("%c", &command);
+		int len = strlen(command)+1;
+
+		// Q또는 q가 들어오면 데이터 송수신을 중단하고 종료한다.
+		if(strcmp(command, 'q') || strcmp(command. 'Q'))
+		{
+
+			// 서버에게 종료 메시지 보내기.
+			if(send(sd,command,len,0) == -1)
+			{
+				// 보내기 실패했다면 오류메시지를 띄우고 다시 메뉴 선택
+				perror("send, reselecte please\n");
+				continue;
+			}
+			break;
+		}
+		switch(command)
+		{
+			case 1:
+				// 첫번째 명령어를 받았다면 내부에서 명령어를 c로 변경하여 서버로 전달
+				char incom = 'c';
+				if(send(sd,incom,len,0) == -1)
+				{
+					// 오류가 발생하면 출력후 처음부터 메뉴 선택
+					perror("send error\n");
+					continue;
+				}
+
+				break;
+			case 2:
+				// 2번째 명령어를 받았다면 내부에서 명령어를 d로 변경하여 서버로 전달
+				char incom = 'd';
+				if(send(sd,incom,len,0) == -1)
+				{
+					// 오류가 발생하면 출력후 처음부터 메뉴 선택
+					perror("send error\n");
+					continue;
+				}
+				break;
+
+			case 3:
+				// 3번째 명령어를 받았다면 내부에서 명령어를 P로 변경하여 서버로 전달
+				char incom = 'P';
+				if(send(sd,incom,len,0) == -1)
+				{
+					// 오류가 발생하면 출력후 처음부터 메뉴 선택
+					perror("send error\n");
+					continue;
+				}
+
+
+				break;
+
+			case 4:
+				// 4번째 명령어를 받았다면 내부에서 명령어를 p로 변경하여 서버로 전달
+				char incom = 'p';
+				if(send(sd,incom,len,0) == -1)
+				{
+					// 오류가 발생하면 출력후 처음부터 메뉴 선택
+					perror("send error\n");
+					continue;
+				}
+
+
+				break;
+
+			default:
+				printf("잘못된 값을 입력하셨습니다. 다시입력해주세요\n");
+				break;
+		}
+
+	}
+    	close(sd);
+
+    	return 0;
+}
+
---
+++ project2/server.c
@@ -0,0 +1,115 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sqlite3.h>
+
+#define SOCKET_NAME		"TodolistProject"
+
+void Create_table(sqlite3* db)
+{
+	sqlite3* db = db;
+	int rc;
+	char* err_msg = 0;
+	
+	// Todolist를 저장하는 database를 열기
+	rc = sqlite3_open("Todolist.db", &db);
+	// 해당 데이터 베이스를 연결 할 수 없다면
+	if(rc !=  SQLITE_OK)
+        {
+                // standard error에 해당 메시지를 출력하고 프로그램을 종료한다.
+                fprintf(stderr, "해당 데이터베이스를 열 수 없습니다. %s\n",sqlite3_errmsg(db));
+                sqlite3_close(db);
+                exit(1);
+        }
+
+	char* query = "CREATE TABLE Lists (Days INT, Name TEXT PRIMARY KEY, Descrition);";
+	
+	rc = sqlite3_exec(db, query, 0, 0, &err_msg);
+	
+	// 명령어 실행이 실패했다면 standard err에 오류를 출력
+	if( rc != SQLITE_OK)
+        {
+                fprintf(stderr, "테이블을 만드는데 실패했습니다.\n");
+                fprintf(stderr, "SQL error: %s\n",err_msg);
+                sqlite3_free(err_msg);
+        }
+}
+
+char Selectall(void* NotUsed, int argc, char** argv, char**azColName)
+{
+	NotUsed = 0;
+
+
+}
+
+
+int main(void)
+{
+	sqlite3* db;
+	sqlite3_stmt* res;
+	
+	struct sockaddr_un ser, cli;
+    	int sd, nsd, len, clen;
+
+	// socket 생성
+    	if ((sd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) 
+	{
+		// 문제가 발생하면 에러를 출력후 종료
+        	perror("socket");
+        	exit(1);
+    	}
+	// 메모리 초기화 및 소켓 구조체에 값을 저장한다.
+    	memset((char *)&ser, 0, sizeof(struct sockaddr_un));
+    	ser.sun_family = AF_UNIX;
+    	strcpy(ser.sun_path, SOCKET_NAME);
+    	len = sizeof(ser.sun_family) + strlen(ser.sun_path);
+
+	// 소켓기술자와 소켈 주소 구조체를 연결한다.
+    	if (bind(sd, (struct sockaddr *)&ser, len)) 
+	{
+		// 오류가 발생하면 에러를 출력후 종료
+        	perror("bind");
+        	exit(1);
+    	}
+
+	// 클라이언트의 접속을 대기한다.
+    	if (listen(sd, 5) < 0) 
+	{
+		// 에러가 발생하면 에러를 출력후 종료
+        	perror("listen");
+        	exit(1);
+    	}
+	
+	// 클라이언트의 접속을 수용한다.
+    	if ((nsd = accept(sd, (struct sockaddr *)&cli, &clen)) == -1) 
+	{
+		// 에러가 발생하면 오류를 출력후 종료
+        	perror("accept");
+        	exit(1);
+    	}
+	while(1)
+	{
+		char command;
+		// 클라이언트가 보낸 메시지를 읽기
+		if (recv(nsd, command, sizeof(command), 0) == -1)
+		{
+			// 오류가 발생했다면 다시 메시지 읽기.
+			perror("recve error\n");
+			continue;
+		}
+		// 만약 클라이언트에서 종료를 보냈다면 서버 종료
+		if(strcmp(command, 'Q') || strcmp(command.'q'))
+			break;
+		if(strcmp(command,
+
+	}
+
+	close(nsd);
+    	close(sd);
+
+	return 0;
+}
