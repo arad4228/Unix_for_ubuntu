4f4b4a4e59fa5ee1bac11f1683485fc14431d57d
arad4228 <arad4228@gmail.com>
2021-12-12 20:10:20 +0900

    완료

--- project2/client.c
+++ project2/client.c
@@ -46,7 +46,8 @@ int main(void)
 		printf("2. 할일 지우기(D)\n");
 		printf("3. 할일 출력하기(모두)(A)\n");
 		printf("4. 할일 출력하기(ID)(S)\n");
-		printf("5. 종료(Q,q)\n"); 
+		printf("5. 오늘 할일출력하기(T)\n");
+		printf("6. 종료(Q,q)\n"); 
 		scanf(" %c", &command);
 		// Q또는 q가 들어오면 데이터 송수신을 중단하고 종료한다.
 		if((command == 'q') || (command == 'Q'))
@@ -60,21 +61,25 @@ int main(void)
 				perror("send, reselecte please\n");
 				exit(1);
 			}
+			printf("프로그램 종료.\n");
 			break;
 		}
-		else if(command == 'N')
+		else if((command == 'N') || (command == 'n'))
 		{	
 			int len;
 			// 첫번째 명령어를 받았다면 내부에서 명령어를 cd로 변경하여 서버로 전달
 			strcpy(incom,"cd");
+			// 서버로 cd를 전달.
 			if(send(sd,incom,sizeof(incom),0) == -1)	
 			{
 				// 오류가 발생하면 출력
-				printf("send error\n");
+				perror("send error\n");
 			}
+			// 서버로 전달할 인자들
 			char days[MAX];
 			char tname[MAX];
-			char tdesct[MAXLEN];	
+			char tdesct[MAXLEN];
+			// 각 인자들에 대한 정보를 받습니다.	
 			printf("Todolist에 저장할 항목을 입력해주세요.\n");
 			printf("날짜를 입력해주세요:");
 			scanf("%s", days);
@@ -84,8 +89,10 @@ int main(void)
 			scanf("%s", tdesct);
 			
 			len = strlen(days);
+			// 끝에 \0을 추가함으로 에러방지
 			days[len+1] = '\0';
 			strcpy(Smsg, days);
+			// 날자에 대한 데이터를 서버로 전달
 			if(send(sd, Smsg,sizeof(Smsg), 0) == -1)
 			{
 				perror("send error1\n");
@@ -93,9 +100,10 @@ int main(void)
 			}
 
 			len= strlen(tname);
+			// 끝에 \0을 추가함으로 에러 방지
 			tname[len+1] = '\0';
 			strcpy(Smsg,tname);
-			// 데이터는 서버로 전달
+			// 이름의 데이터를 서버로 전달
 			if(send(sd, Smsg,sizeof(Smsg),0) == -1)
 			{
 				// 오류발생시 종료.
@@ -104,53 +112,73 @@ int main(void)
 			}
 			
 			len = strlen(tdesct);
+			// 끝에 \0을 추가함으로써 에러방지
 			tdesct[len+1] = '\0';
 			strcpy(Smsg, tdesct);
+			
+			// 세부설명에 대한 데이터를 서버로 전달
+			printf("서버로 데이터를 전달합니다....\n");
 			if(send(sd,Smsg,sizeof(Smsg),0) == -1)
 			{
+				// 오류가나면출력후 종료
 				perror("send error3\n");
 				exit(1);
 			}
 			
+			printf("서버로 전달이 완료되고 결과를 받는 중입니다....\n");
+			// 서버로부터 받는 결과 메시지를 받는다.
 			if(recv(sd, Rmsg, sizeof(Rmsg),0) == -1)
 			{
+				// 오류발생시 출력후 종료
 				printf("recv fail\n");
 				exit(1);
 			}
+			// 추가에 대한 결과메시지를 출력한다.
 			printf("%s\n",Rmsg);
 		}
-		else if(command == 'D')
+		else if((command == 'D') || (command == 'd'))
 		{
 			int len;
 			// 2번째 명령어를 받았다면 내부에서 명령어를 dd로 변경하여 서버로 전달
 			strcpy(incom, "dd");
+			// 서버로 dd를 전달
 			if(send(sd,incom,sizeof(incom),0) == -1)
 			{
-				// 오류가 발생하면 출력후 처음부터 메뉴 선택
+				// 오류가 발생하면 출력후 종료
 				printf("send error\n");
+				exit(1);
 			}
+
 			printf("TOdolist에서 삭제할 일정ID을 입력하세요.");
 			scanf("%s", Smsg);
 			len = strlen(Smsg);
+			// 에러를 방지하기 위해 \0을 넣는다.
 			Smsg[len+1] = '\0';
-
+			
+			printf("서버로 데이터를 전달하는 중입니다....\n");
 			// 서버로 이름을 보내서 내용을 제거하기
 			if(send(sd, Smsg,sizeof(Smsg),0) == -1)
-			{	// 오류가 발생하면 출력 후 처음부터 시작
+			{	// 오류가 발생하면 출력 후 종료.
 				printf("send error\n");
+				exit(1);
 			}
+			printf("서버로 데이터 전달이 완료되었고 서버로부터 데이터를 받는 중입니다...\n");
+			// 서버로부터 데이터를 받는디.
 			if(recv(sd, Rmsg, sizeof(Rmsg), 0) == -1)
 			{
 				printf("recv error\n");
 				exit(1);
 			}
+			
+			// 삭제 결과에 대해 출력
 			printf("%s\n",Rmsg);
 	
 		}
-		else if(command == 'A')
+		else if((command == 'A') || (command == 'a'))
 		{	
 			// 3번째 명령어를 받았다면 내부에서 명령어를 Pd로 변경하여 서버로 전달
 			strcpy(incom, "Pd");
+			// 서버로 Pd를 전달
 			if(send(sd,incom,sizeof(incom),0) == -1)
 			{
 				// 오류가 발생하면 출력후 처음부터 메뉴 선택
@@ -161,48 +189,109 @@ int main(void)
 			if(recv(sd, Rmsg,sizeof(Rmsg),0) == -1)
 			{
 				printf("recv error\n");
+				exit(1);
 			}
+			
+			// 전달받은 값을 int로 변환해서 저장
 			numberofRow = atoi(Rmsg);
-			printf("%d\n",numberofRow);
+			
+			// 만약 출력할 값이 없다면아래를 출력 그렇지 않으면
 			if (numberofRow == 0)
 			{
 				printf("데이터베이스에 아무것도 없습니다.\n");
 			}
+			// row수만큼서버로 데이터를 받아 반복해서 출력을 한다.
 			for(int i = 0; i < numberofRow; i++)
 			{
+				// 서버로부터 데이터를 전달받는다.
 				if(recv(sd, Rmsg, sizeof(Rmsg),0) == -1)
 				{
+					// 오류발생시 출력후 종료
 					printf("recv error\n");
 					exit(1);
 				}
 				printf("%s\n",Rmsg);
 			}
 		}
-		else if(command == 'S')
+		else if((command == 'S') || (command == 's'))
 		{
 			// 4번째 명령어를 받았다면 내부에서 명령어를 pd로 변경하여 서버로 전달
 			strcpy(incom, "pd");
+			// 서버로 pd를 전달
 			if(send(sd,incom,sizeof(incom),0) == -1)
 			{
-				// 오류가 발생하면 출력후 처음부터 메뉴 선택
-				printf("send error\n");
+				// 오류가 발생하면 종료
+				perror("send error\n");
+				exit(1);
 			}
 			printf("Todolist에서 출력하길 원하는 Id을 입력하세요.");
 			scanf("%s", Smsg);
-		
-			// 삭제할 이름을서버로 보낸다
+			printf("서버로 데이터를 전달 중 입니다.\n");
+
+			// 삭제할 id을서버로 보낸다
 			if(send(sd, Smsg, sizeof(Smsg),0) == -1)
 			{
-				// 문제가 있다면 출력.
-				printf("send error\n");
+				// 문제가 있다면 출력후 종료.
+				perror("send error\n");
+				exit(1);
 			}
-
+			
+			printf("서버로 전달이 완료되었고 서버로부터 데이터를 받는중입니다....\n");
+			// 서버로부터 삭제 결과에 대한 정보를 받는다.
 			if(recv(sd, Rmsg, sizeof(Rmsg),0) == -1)
 			{
-				printf("recv erro\n");
+				// 오류발생시 출력후 종료
+				perror("recv erro\n");
+				exit(1);
 			}
 			printf("%s\n",Rmsg);
 		}
+		else if((command == 'T') || (command == 't'))
+		{
+			int numberofRow;
+			// 5번째 명령어를 받았ㄲ다면 내부에서 명령어를 td로 변경하여 서버로 전달
+			strcpy(incom, "td");
+			// 서버에게 td를 전달
+			if(send(sd, incom, sizeof(incom),0) == -1)
+			{
+				perror("send fail\n");
+				exit(1);
+			}
+
+			printf("서버에서 데이터를 받아오는 중입니다.....\n");
+			// 서버로부터 row의 갯수를 받아온다.
+			if(recv(sd, Rmsg, sizeof(Rmsg), 0) == -1)
+			{
+				// 오류가 있다면 출력후 종료
+				perror("recv error\n");
+				exit(1);
+			}
+			numberofRow = atoi(Rmsg);
+			
+			// 출력할 값이 없다면 아래를 출력그렇지 않으면
+			if( numberofRow == 0)
+			{
+				printf("오늘 해야할 일은 없습니다.\n");
+			}
+			// row의 갯수만큼 서버로부터 받아와 출력한다.
+			else
+			{
+				for(int i =0; i < numberofRow; i++)
+				{
+					// 서버로부터 데이터를 받고 출력
+					if(recv(sd, Rmsg, sizeof(Rmsg),0) == -1)
+					{
+						// 오류가 나면 출력후 종료
+						perror("recv error\n");
+						exit(1);
+					}
+					printf("%s\n",Rmsg);
+				}
+			}
+
+
+		}
+		// 모든 조건을 맞추지 않은 잘못입력한 경우
 		else
 		{
 			printf("잘못된 값을 입력하셨습니다. 다시입력해주세요\n");
--- project2/server.c
+++ project2/server.c
@@ -1,4 +1,5 @@
 #include <sqlite3.h>
+#include <time.h>
 #include "Todolist.h"
 
 #define SOCKET_NAME		"TodolistProject"
@@ -9,13 +10,21 @@ int main(void)
 	int rc;
 	sqlite3_stmt* res;
 	int step;
-	int no = 0;	
+	char* err_msg = 0;
+
 	struct sockaddr_un ser, cli;
     	int sd, nsd, len, clen;
 	// 받는 메시지
 	char Rmsg[TotalMax];
 	// 보낼 메시지
 	char Smsg[TotalMax];
+
+	// 시간데이터를 받아올 구조제와 데이터연동
+	time_t t;
+	struct tm* tm;
+	time(&t);
+	tm = localtime(&t);
+
 
 	// socket 생성
     	if ((sd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) 
@@ -24,6 +33,7 @@ int main(void)
         	perror("socket");
         	exit(1);
     	}
+
 	// 메모리 초기화 및 소켓 구조체에 값을 저장한다.
     	memset((char *)&ser, 0, sizeof(struct sockaddr_un));
     	ser.sun_family = AF_UNIX;
@@ -46,7 +56,8 @@ int main(void)
         	perror("listen");
         	exit(1);
     	}
-	
+
+	printf("클라이언트 접속 수용\n");
 	// 클라이언트의 접속을 수용한다.
     	if ((nsd = accept(sd, (struct sockaddr *)&cli, &clen)) == -1) 
 	{
@@ -54,11 +65,9 @@ int main(void)
         	perror("accept");
         	exit(1);
     	}
+
 	printf("클라이언트 접속 완료\n");
 	// 데이터 베이스 생성
-	char* err_msg = 0;
-
-        // Todolist를 저장하는 database를 열기
         rc = sqlite3_open("Todolist.db", &db);
         // 해당 데이터 베이스를 연결 할 수 없다면
         if(rc !=  SQLITE_OK)
@@ -68,10 +77,12 @@ int main(void)
                 sqlite3_close(db);
                 exit(1);
         }
+	// 데이터 베이스 생성 쿼리문과 기본적인 데이터를 저장.
         char* query = "DROP TABLE IF EXISTS Lists;"
                 "CREATE TABLE Lists (ID INTEGER PRIMARY KEY, EndDay INTEGER, Name TEXT, Description TEXT);"
 		"INSERT INTO Lists VALUES (1, 20,'유닉스', '시험');";
 
+	// Sqlite query문 실행
         rc = sqlite3_exec(db, query, 0, 0, &err_msg);
 
         // 명령어 실행이 실패했다면 standard err에 오류를 출력
@@ -92,62 +103,81 @@ int main(void)
 			perror("recve error\n");
 			exit(1);
 		}
+		// 받은 명령문을 출력
 		printf("%s\n", command);
+
 		// 만약 클라이언트에서 종료를 보냈다면 서버 종료
 		if(strcmp(command,"qq")== 0)
 		{
 			break;
 		}
-		// 데이터 집어넣기
+		// 데이터 insert를 호출했다면
 		if(strcmp(command,"cd") == 0)
 		{
+			// 현재 지금 어떠한 명령을 실행중인지 출력
 			printf("ok cd\n");
+
 			int day;
 			char Name[MAX];
 			char Desct[MAXLEN];
+
+			// SQLITE3 쿼리문 생성
 			char* query = "INSERT INTO Lists(EndDay ,Name, Description) VALUES(?,?,?)";
-			// client에서 보낸 메시지를 받기
 			
+			// client에서 보낸 day정보를 받기	
 			if(recv(nsd, Rmsg, sizeof(Rmsg), 0) == -1)
 			{
+				// 오류가 발생하면 출력후 종효
 				perror("recv error1\n");
 				exit(1);
 			}
+			// day를 integer로 변경
 			day = atoi(Rmsg);
 
+			// client에서 보낸 name정보 받기 
 			if(recv(nsd, Rmsg, sizeof(Rmsg), 0) == -1)
 			{     
-				// 오류가 발생하면 에러를 출력하고 다시 메시지 읽기
+				// 오류가 발생하면 에러를 출력하고 종료
 				perror("recv error2\n");
 				exit(1);
 			}
+			// 정보 복사
 			strcpy(Name,Rmsg);
 
+			// client로부터 세부설명 정보 받기
 			if(recv(nsd, Rmsg, sizeof(Rmsg), 0) == -1)
 			{
 				perror("recv error3\n");
 				exit(1);
 			}
+			// 정보 복사
 			strcpy(Desct, Rmsg);
-			printf("%s %s\n",Name, Desct);
 
+			// SQLITE query문 준비
 			rc = sqlite3_prepare_v2(db,query, -1, &res, 0);
+			// 제대로 생성됬다면
 			if( rc == SQLITE_OK)
 			{
+				// 쿼리문의 ?변수에 값 연동하기
 				sqlite3_bind_int(res,1, day);
 				sqlite3_bind_text(res,2,Name, -1, SQLITE_TRANSIENT);
 				sqlite3_bind_text(res,3,Desct,-1, SQLITE_TRANSIENT);
+				// 쿼리문 동작
 				step = sqlite3_step(res);
 				strcpy(Smsg,"저장이 완료되었습니다.\n");
-                                if(send(nsd, Smsg, sizeof(Smsg),0) == -1)
+                                // 쿼리문이 제대로 되었기에 이에 대한 정보를 client에게 전송.
+				if(send(nsd, Smsg, sizeof(Smsg),0) == -1)
                                 {
+					// 오류발생시 출력후 종료
                                         perror("send error\n");
                                         exit(1);
                                 }
 
 			}
+			// 만약 문제가 발생했다면
 			else
 			{
+				// 저장에 실패함을 client에게 전달
 				strcpy(Smsg, "저장에 실패했습니다\n");
 				if(send(nsd, Smsg, sizeof(Smsg),0) == -1)
 				{
@@ -155,120 +185,230 @@ int main(void)
 					exit(1);
 				}
 			}
-                        }
-		// 데이터 삭제
+			// 쿼리 비우기.
+			sqlite3_finalize(res);
+		}
+		// 데이터 delete를 호출했다면
 		if(strcmp(command,"dd") == 0)
 		{
 			int id;
+			// 현재 command를 출력한다.
 			printf("ok dd\n");
+			// client로부터 삭제할 정보의 데이터를 받는다.
 			if(recv(nsd, Rmsg, sizeof(Rmsg), 0) == -1)
 			{
+				// 실패했다면 오류를 출력후 종료
 				perror("recve error\n");
 				exit(1);
 			}
-			printf("%s\n",Rmsg);
+			// 받은 데이터를 변환하기
 			id = atoi(Rmsg);
+			// SQLITE 쿼리문 만들기
 			char* query = "DELETE FROM Lists WHERE ID = ?";
+			// 쿼리문 준비하기
 			rc = sqlite3_prepare_v2(db,query, -1, &res, 0);
 
+			// 정상적으로 만들어졌다면
 			if( rc == SQLITE_OK)
 			{
+				// ?에 값을 연동한다.
 				sqlite3_bind_int(res,1,id);
 			}
+			// 쿼리문을 실행한다.
 			step = sqlite3_step(res);
+			// 실행이되면 client에게 결과를 전달한다.
 			strcpy(Smsg,"삭제가 완료되었습니다.\n");
+			// 결과를 전달
 			if( send(nsd,Smsg, sizeof(Smsg),0) == -1)
 			{
+				// 오류가 발생하면 출력후 종료.
 				perror("send to fail");
 				exit(1);
 			}
+			// 쿼리 비우기.
 			sqlite3_finalize(res);
 
 		}
-		// 데이터 모두 출력
+		// 데이터 모두 출력을 받았을 경우
 		if (strcmp(command,"Pd") == 0)
 		{
+			// 현재 행하는 동작을 출력
 			printf("ok Pd\n");
 			int count;
+			// SQLITE 쿼리문 생성
 			char* query1 = "SELECT COUNT(Id) FROM Lists";
+			// 쿼리문 준비
 			rc = sqlite3_prepare_v2(db,query1,-1,&res,0);
+			// 문제가 없다면 진행 있다면 에러를 출력후 종료
 			if(rc != SQLITE_OK)
 			{
 				perror("query error1\n");
 				exit(1);
 			}
+			// 쿼리문 실행
 			step = sqlite3_step(res);
 
 			// 현재 몇개의 row가있는지 전송
 			if(step == SQLITE_ROW)
 			{
 				sprintf(Smsg,"%d", sqlite3_column_int(res,0));
-				count = atoi(Smsg);
+				// 쿼리문을 몇번 반복해서 돌릴 지에 대해 저장
+				count = atoi(Smsg);                        
+				
+				// 데이터를 client로 전달
 				if(send(nsd,Smsg, sizeof(Smsg), 0) == -1)
 				{
 					perror("send error");
 				}
 			}
-
+			// 쿼리문 비우기
 			sqlite3_finalize(res);
 
-			// 모든 데이터를 전송하기.
+			// 모든 데이터를 전송하기하기위한 쿼리문 생성
 			char* query2 = "SELECT * FROM Lists";
+			// 쿼리문 준비하기
 			rc = sqlite3_prepare_v2(db,query2,-1,&res,0);
+			// 오류가 없다면 진행 있다면 에러를 출력후 종료
 			if(rc != SQLITE_OK)
 			{
 				perror("query error2\n");
 				exit(1);
 			}
+			// SQL에서 전달해줄 ROW가 남아있는 동안
 			while(sqlite3_step(res) == SQLITE_ROW)
 			{
+				// 해당 문을 저장해서 클라이언트로 전달한다.
 				sprintf(Smsg,"ID: %d, EndDay: %d, 이름: %s, 세부내용: %s\n",
 				sqlite3_column_int(res,0), sqlite3_column_int(res,1), sqlite3_column_text(res,2),
 				sqlite3_column_text(res,3));
-                                printf("%s",Smsg);
+				// 데이터 전달.
 				if(send(nsd, Smsg, sizeof(Smsg),0) == -1)
                                         printf("send error\n");
 			}
 
+			// 쿼리문 비우기.
 			sqlite3_finalize(res);
 		}
-		// Id으로 원하는 데이터 출력
+		// Id으로 원하는 데이터 출력을 전달받은 경우
 		if (strcmp(command,"pd") == 0)
 		{
 			int id;
+			// 현재 진행중인 동작을 출력
 			printf("ok pd\n");
+			// client로부터 데이터를 전달 받는다.
 			if(recv(nsd,Rmsg,sizeof(Rmsg),0) == -1)
 			{
 				printf("rece error\n");
 				exit(1);
 			}
 			id = atoi(Rmsg);
-			printf("%s\n",Rmsg);
+			// 쿼리문을 생성한다.
 			char* query = "SELECT * FROM Lists WHERE ID = ?";
+			// 쿼리문 준비하기
 			rc = sqlite3_prepare_v2(db,query, -1, &res, 0);
+			// 쿼리문에 문제가 없다면
 			if( rc == SQLITE_OK)
 			{
+				// ?에 대한 값을 연동한다.
 				sqlite3_bind_int(res,1,id);
 			}
+			// 쿼리문을 실행한다.
 			step = sqlite3_step(res);
-
+		
+			// 쿼리문에서 받아올 값이 있다면
 			if(step ==  SQLITE_ROW)
 			{
+				// 해당 형식에 맞게 변환하여 client로 전달한다.
 				sprintf(Smsg,"ID: %d, EndDay: %d, 이름: %s, 세부내용: %s\n",sqlite3_column_int(res,0), sqlite3_column_int(res,1),sqlite3_column_text(res,2), sqlite3_column_text(res,4));
+				// client로 전달
 				if(send(nsd, Smsg, sizeof(Smsg),0) == -1)
-					printf("send error\n");
+				{
+					perror("send error\n");
+					exit(1);
+				}
 			}
+			// 그렇지 않다면 == 없다면
 			else
 			{
+				// 없다는 결과를 전송한다.
 				strcpy(Smsg,"아무런 데이터가 없습니다.");
+				// 결과를 전송한다.
 				if(send(nsd, Smsg, sizeof(Smsg),0) == -1)
+				{
 					printf("send erro\n");
+					exit(1);
+				}
 			}
+			// 쿼리문 정리하기
 			sqlite3_finalize(res);
+		}
+		// 오늘 할일 출력을 전달받았다면
+		if (strcmp(command, "td") == 0)
+		{
+			int days;
+			int count;
+                        printf("ok td\n");
+			// 시간구조체에서 월에 대한 정보 받아오기.
+                        days = tm->tm_mon+1;
+			
+			// 1번째 쿼리문 작성하기(몇개의 개수가 있는지 전달을 위한)
+			char* query1 = "SELECT COUNT(ID) FROM Lists WHERE EndDay = ?";
+                        // 쿼리문을 준비하기
+			rc = sqlite3_prepare_v2(db,query1, -1, &res, 0);
+                        // 문제가 없다면
+			if( rc == SQLITE_OK)
+                        {
+				// ?에  값을 연동한다
+                                sqlite3_bind_int(res,1,days);
+                        }
+			// 쿼리문을 실행한다.
+                        step = sqlite3_step(res);
+
+			// 쿼리문에서 받아올 값이 있다면
+                        if(step ==  SQLITE_ROW)
+                        {
+				// 즉 row가 있다면 이를 client에게 전달
+				sprintf(Smsg,"%d", sqlite3_column_int(res,0));
+				count = atoi(Smsg);
+				// client로 전달
+				if(send(nsd, Smsg, sizeof(Smsg),0) == -1)
+				{
+					perror("send error\n");
+					exit(1);
+				}
+                        }
+			// 쿼리 비우기
+                        sqlite3_finalize(res);
+
+			// 2번째 쿼리 작성하기(전체 데이터 가져오기위한)
+			char* query2 = "SELECT * FROM Lists WHERE EndDay = ?";
+			// 쿼리문 준비하기
+			rc = sqlite3_prepare_v2(db,query2,-1,&res,0);
+			// 문제가 없다면
+			if( rc == SQLITE_OK)
+			{
+				// ?에 값 연동하기.
+				sqlite3_bind_int(res,1,days);
+			}
+			// SQL에서 전달할 데이터가 있는 동안
+			while(sqlite3_step(res) == SQLITE_ROW)
+			{
+				// 형식에 맞게 작성해서 client에게 전달
+				sprintf(Smsg, "ID: %d, EndDay: %d, 이름: %s, 세부내용: %s", sqlite3_column_int(res,0), sqlite3_column_int(res,1), sqlite3_column_text(res,2),sqlite3_column_text(res,3));
+				// client로 데이터 전달.
+				if(send(nsd, Smsg, sizeof(Smsg), 0) == -1)
+				{
+					perror("send error\n");
+				}
+			}
+			// 쿼리 비우기.
+			sqlite3_finalize(res);
+
 		}
 
 	}
-
+	
+	// SQL을 닫고 모든 소켓을 닫고 종료한다.
 	sqlite3_close(db);
 	close(nsd);
     	close(sd);
